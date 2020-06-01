30965f0f008660771f250ce359cece8a3d7f0524
kgcdream2019 <kgc.dream2019@gmail.com>
2020-06-01 16:32:42 +0800

    add libp2p wrapper

---
+++ common/libp2p/multi/cbor_multiaddress.hpp
@@ -0,0 +1,34 @@
+#ifndef CPP_IPFS_LITE_COMMON_LIBP2P_MULTI_CBOR_MULTIADDRESS_HPP
+#define CPP_IPFS_LITE_COMMON_LIBP2P_MULTI_CBOR_MULTIADDRESS_HPP
+
+#include <libp2p/multi/multiaddress.hpp>
+
+#include "codec/cbor/streams_annotation.hpp"
+
+using libp2p::multi::Multiaddress;
+
+namespace ipfs_lite::codec::cbor {
+  /// Default value of Multiaddress for CBOR stream decoder
+  template <>
+  inline Multiaddress kDefaultT<Multiaddress>() {
+    return Multiaddress::create("/ip4/0.0.0.1/udp/1").value();
+  };
+}  // namespace ipfs_lite::codec::cbor
+
+namespace libp2p::multi {
+
+  CBOR_ENCODE(Multiaddress, ma) {
+    return s << ma.getBytesAddress();
+  }
+
+  CBOR_DECODE(Multiaddress, ma) {
+    std::vector<uint8_t> bytes;
+    s >> bytes;
+    OUTCOME_EXCEPT(created, Multiaddress::create(bytes));
+    ma = std::move(created);
+    return s;
+  }
+
+}  // namespace libp2p::multi
+
+#endif  // CPP_IPFS_LITE_COMMON_LIBP2P_MULTI_CBOR_MULTIADDRESS_HPP
---
+++ common/libp2p/peer/cbor_peer_id.hpp
@@ -0,0 +1,38 @@
+#ifndef CPP_IPFS_LITE_COMMON_LIBP2P_PEER_CBOR_PEER_ID_HPP
+#define CPP_IPFS_LITE_COMMON_LIBP2P_PEER_CBOR_PEER_ID_HPP
+
+#include <libp2p/peer/peer_id.hpp>
+
+#include "codec/cbor/streams_annotation.hpp"
+#include "common/outcome.hpp"
+
+using libp2p::peer::PeerId;
+
+namespace ipfs_lite::codec::cbor {
+  /// Default value of PeerId for CBOR stream decoder
+  template <>
+  inline PeerId kDefaultT<PeerId>() {
+    return PeerId::fromHash(
+               libp2p::multi::Multihash::create(libp2p::multi::sha256, {})
+                   .value())
+        .value();
+  }
+}  // namespace ipfs_lite::codec::cbor
+
+namespace libp2p::peer {
+
+  CBOR_ENCODE(PeerId, peer) {
+    return s << peer.toVector();
+  }
+
+  CBOR_DECODE(PeerId, peer) {
+    std::vector<uint8_t> bytes;
+    s >> bytes;
+    OUTCOME_EXCEPT(peer_id, PeerId::fromBytes(bytes));
+    peer = std::move(peer_id);
+    return s;
+  }
+
+}  // namespace libp2p::peer
+
+#endif  // CPP_IPFS_LITE_COMMON_LIBP2P_PEER_CBOR_PEER_ID_HPP
