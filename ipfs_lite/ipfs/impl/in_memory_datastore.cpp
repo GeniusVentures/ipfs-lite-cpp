9b786f3339f978e8820b1a8ea009938b0d186391
kgcdream2019 <kgc.dream2019@gmail.com>
2020-06-03 21:47:24 +0800

    ipfs submodule compile error fix

--- ipfs_lite/ipfs/impl/in_memory_datastore.cpp
+++ ipfs_lite/ipfs/impl/in_memory_datastore.cpp
@@ -1,25 +1,26 @@
 
 #include "ipfs_lite/ipfs/impl/in_memory_datastore.hpp"
-
+namespace sgns::ipfs_lite::ipfs {
 using sgns::ipfs_lite::ipfs::InMemoryDatastore;
 using Value = sgns::ipfs_lite::ipfs::IpfsDatastore::Value;
 
-sgns::outcome::result<bool> InMemoryDatastore::contains(const CID &key) const {
-  return storage_.find(key) != storage_.end();
-}
+    sgns::outcome::result<bool> InMemoryDatastore::contains(const CID &key) const {
+    return storage_.find(key) != storage_.end();
+    }
 
-sgns::outcome::result<void> InMemoryDatastore::set(const CID &key, Value value) {
-  storage_[key] = value;
-  return sgns::outcome::success();
-}
+    sgns::outcome::result<void> InMemoryDatastore::set(const CID &key, Value value) {
+    storage_[key] = value;
+    return sgns::outcome::success();
+    }
 
-sgns::outcome::result<Value> InMemoryDatastore::get(const CID &key) const {
-  OUTCOME_TRY(found, contains(key));
-  if (!found) return IpfsDatastoreError::NOT_FOUND;
-  return storage_.at(key);
-}
+    sgns::outcome::result<Value> InMemoryDatastore::get(const CID &key) const {
+    OUTCOME_TRY(found, contains(key));
+    if (!found) return IpfsDatastoreError::NOT_FOUND;
+    return storage_.at(key);
+    }
 
-sgns::outcome::result<void> InMemoryDatastore::remove(const CID &key) {
-  storage_.erase(key);
-  return sgns::outcome::success();
+    sgns::outcome::result<void> InMemoryDatastore::remove(const CID &key) {
+    storage_.erase(key);
+    return sgns::outcome::success();
+    }
 }
