ee99124148bc57bcba12eb2ad6ccf11c1b9990a5
kgcdream2019 <kgc.dream2019@gmail.com>
2020-06-04 19:51:00 +0800

    ipfs_lite ExternalProject_Add last commit

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -4,3 +4,65 @@ function(disable_clang_tidy target)
       CXX_CLANG_TIDY ""
       )
 endfunction()
+
+
+function(compile_proto_to_cpp PB_H PB_CC PROTO)
+  get_target_property(Protobuf_INCLUDE_DIR protobuf::libprotobuf INTERFACE_INCLUDE_DIRECTORIES)
+  get_target_property(Protobuf_PROTOC_EXECUTABLE protobuf::protoc IMPORTED_LOCATION_RELEASE)
+
+  if (NOT Protobuf_PROTOC_EXECUTABLE)
+    message(FATAL_ERROR "Protobuf_PROTOC_EXECUTABLE is empty")
+  endif ()
+  if (NOT Protobuf_INCLUDE_DIR)
+    message(FATAL_ERROR "Protobuf_INCLUDE_DIR is empty")
+  endif ()
+
+  get_filename_component(PROTO_ABS "${PROTO}" REALPATH)
+  # get relative (to CMAKE_BINARY_DIR) path of current proto file
+  file(RELATIVE_PATH SCHEMA_REL "${CMAKE_BINARY_DIR}/core" "${CMAKE_CURRENT_BINARY_DIR}")
+  set(SCHEMA_OUT_DIR ${CMAKE_BINARY_DIR}/generated)
+  file(MAKE_DIRECTORY ${SCHEMA_OUT_DIR})
+
+  string(REGEX REPLACE "\\.proto$" ".pb.h" GEN_PB_HEADER ${PROTO})
+  string(REGEX REPLACE "\\.proto$" ".pb.cc" GEN_PB ${PROTO})
+
+  set(GEN_COMMAND ${Protobuf_PROTOC_EXECUTABLE})
+  set(GEN_ARGS ${Protobuf_INCLUDE_DIR})
+
+  add_custom_command(
+      OUTPUT ${SCHEMA_OUT_DIR}/${SCHEMA_REL}/${GEN_PB_HEADER} ${SCHEMA_OUT_DIR}/${SCHEMA_REL}/${GEN_PB}
+      COMMAND ${GEN_COMMAND}
+      ARGS -I${PROJECT_SOURCE_DIR}/core -I${GEN_ARGS} --cpp_out=${SCHEMA_OUT_DIR} ${PROTO_ABS}
+      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+      DEPENDS protobuf::protoc
+      VERBATIM
+  )
+
+  set(${PB_H} ${SCHEMA_OUT_DIR}/${SCHEMA_REL}/${GEN_PB_HEADER} PARENT_SCOPE)
+  set(${PB_CC} ${SCHEMA_OUT_DIR}/${SCHEMA_REL}/${GEN_PB} PARENT_SCOPE)
+endfunction()
+
+add_custom_target(generated
+    COMMENT "Building generated files..."
+    )
+
+function(add_proto_library NAME)
+  set(SOURCES "")
+  foreach (PROTO IN ITEMS ${ARGN})
+    compile_proto_to_cpp(H C ${PROTO})
+    list(APPEND SOURCES ${H} ${C})
+  endforeach ()
+
+  add_library(${NAME}
+      ${SOURCES}
+      )
+  target_link_libraries(${NAME}
+      protobuf::libprotobuf
+      )
+  target_include_directories(${NAME} PUBLIC
+      ${CMAKE_BINARY_DIR}/generated/
+      )
+  disable_clang_tidy(${NAME})
+
+  add_dependencies(generated ${NAME})
+endfunction()
