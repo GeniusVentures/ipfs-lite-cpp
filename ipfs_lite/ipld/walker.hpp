f68ce6dd3791879c52823d4b647ade0f4362ad17
kgcdream2019 <kgc.dream2019@gmail.com>
2020-06-05 23:09:50 +0800

    added ipld/walker module and fixed compiling errors

---
+++ ipfs_lite/ipld/walker.hpp
@@ -0,0 +1,31 @@
+
+#ifndef CPP_IPFS_LITE_STORAGE_IPLD_WALKER_HPP
+#define CPP_IPFS_LITE_STORAGE_IPLD_WALKER_HPP
+
+#include <set>
+
+#include "ipfs_lite/ipfs/datastore.hpp"
+
+namespace sgns::ipfs_lite::ipld::walker {
+  using codec::cbor::CborDecodeStream;
+  using Ipld = ipfs::IpfsDatastore;
+
+  // TODO(turuslan): implement selectors
+  struct Selector {};
+
+  struct Walker {
+    Walker(Ipld &store) : store{store} {}
+
+    outcome::result<void> select(const CID &root, const Selector &selector);
+
+    outcome::result<void> recursiveAll(const CID &cid);
+
+    void recursiveAll(CborDecodeStream &s);
+
+    Ipld &store;
+    std::set<CID> visited;
+    std::vector<CID> cids;
+  };
+}  // namespace sgns::ipfs_lite::ipld::walker
+
+#endif  // CPP_IPFS_LITE_STORAGE_IPLD_WALKER_HPP
