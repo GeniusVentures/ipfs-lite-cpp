54dde40bc0e335f1fb4cf19dcd5d3b652dfe7d1d
kgcdream2019 <kgc.dream2019@gmail.com>
2020-06-08 10:39:33 +0800

    added merkledag module

---
+++ ipfs_lite/ipfs/merkledag/impl/leaf_impl.cpp
@@ -0,0 +1,51 @@
+
+#include "ipfs_lite/ipfs/merkledag/impl/leaf_impl.hpp"
+
+namespace sgns::ipfs_lite::ipfs::merkledag {
+  LeafImpl::LeafImpl(common::Buffer data)
+      : content_{std::move(data)} {}
+
+  const common::Buffer &LeafImpl::content() const {
+    return content_;
+  }
+
+  size_t LeafImpl::count() const {
+    return children_.size();
+  }
+
+  outcome::result<std::reference_wrapper<const Leaf>> LeafImpl::subLeaf(
+      std::string_view name) const {
+    if (auto iter = children_.find(name); iter != children_.end()) {
+      return iter->second;
+    }
+    return LeafError::LEAF_NOT_FOUND;
+  }
+
+  std::vector<std::string_view> LeafImpl::getSubLeafNames() const {
+    std::vector<std::string_view> names;
+    for (const auto &child : children_) {
+      names.push_back(child.first);
+    }
+    return names;
+  }
+
+  outcome::result<void> LeafImpl::insertSubLeaf(std::string name,
+                                                 LeafImpl children) {
+    auto result = children_.emplace(std::move(name), std::move(children));
+    if (result.second) {
+      return outcome::success();
+    }
+    return LeafError::DUPLICATE_LEAF;
+  }
+}  // namespace sgns::ipfs_lite::ipfs::merkledag
+
+OUTCOME_CPP_DEFINE_CATEGORY_3(sgns::ipfs_lite::ipfs::merkledag, LeafError, e) {
+  using sgns::ipfs_lite::ipfs::merkledag::LeafError;
+  switch (e) {
+    case (LeafError::LEAF_NOT_FOUND):
+      return "MerkleDAG leaf: children leaf not found";
+    case (LeafError::DUPLICATE_LEAF):
+      return "MerkleDAG leaf: duplicate leaf name";
+  }
+  return "MerkleDAG leaf: unknown error";
+}
---
+++ ipfs_lite/ipfs/merkledag/merkledag_service.hpp
@@ -0,0 +1,85 @@
+#ifndef CPP_IPFS_LITE_IPFS_MERKLEDAG_SERVICE_HPP
+#define CPP_IPFS_LITE_IPFS_MERKLEDAG_SERVICE_HPP
+
+#include <memory>
+
+#include "common/outcome.hpp"
+#include "ipfs_lite/ipfs/merkledag/leaf.hpp"
+#include "ipfs_lite/ipld/ipld_node.hpp"
+
+namespace sgns::ipfs_lite::ipfs::merkledag {
+  using ipld::IPLDNode;
+
+  class MerkleDagService {
+   public:
+    /**
+     * @brief Destructor
+     */
+    virtual ~MerkleDagService() = default;
+
+    /**
+     * @brief Add new node
+     * @param node - entity to add
+     * @return operation result
+     */
+    virtual outcome::result<void> addNode(std::shared_ptr<const IPLDNode> node) = 0;
+
+    /**
+     * @brief Get node by id
+     * @param cid - node id
+     * @return operation result
+     */
+    virtual outcome::result<std::shared_ptr<IPLDNode>> getNode(
+        const CID &cid) const = 0;
+
+    /**
+     * @brief Remove node by id
+     * @param cid - node id
+     * @return operation result
+     */
+    virtual outcome::result<void> removeNode(const CID &cid) = 0;
+
+    /**
+     * @brief Get nodes with IPLD-selector
+     * @param root_cid - bytes of the root Node CID
+     * @param selector - IPLD-selector raw bytes
+     * @param handler - receiver of the selected Nodes, should return false to
+     * break receiving process
+     * @return count of the received by handler Nodes
+     */
+    virtual outcome::result<size_t> select(
+        gsl::span<const uint8_t> root_cid,
+        gsl::span<const uint8_t> selector,
+        std::function<bool(std::shared_ptr<const IPLDNode> node)> handler)
+        const = 0;
+
+    /**
+     * @brief Fetcg graph from given root node
+     * @param cid - identifier of the root node
+     * @return operation result
+     */
+    virtual outcome::result<std::shared_ptr<Leaf>> fetchGraph(
+        const CID &cid) const = 0;
+
+    /**
+     * @brief Construct graph from given root node till chosen limit
+     *        (0 means "Fetch only root node")
+     * @param cid - identifier of the root node
+     * @param depth - limit of the depth to fetch
+     * @return operation result
+     */
+    virtual outcome::result<std::shared_ptr<Leaf>> fetchGraphOnDepth(
+        const CID &cid, uint64_t depth) const = 0;
+  };
+
+  /**
+   * @class Possible MerkleDAG service errors
+   */
+  enum class ServiceError {
+    UNRESOLVED_LINK = 1  // This error can occur if child node not found
+  };
+}  // namespace sgns::ipfs_lite::ipfs::merkledag
+
+OUTCOME_HPP_DECLARE_ERROR_2(sgns::ipfs_lite::ipfs::merkledag, ServiceError)
+
+#endif
