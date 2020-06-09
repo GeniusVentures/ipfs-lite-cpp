11b48444e460bc1b94dfdf78b3e82c521b86ce4f
kgcdream2019 <kgc.dream2019@gmail.com>
2020-06-09 16:41:09 +0800

    compile error fix

--- common/todo_error.cpp
+++ common/todo_error.cpp
@@ -1,6 +1,6 @@
 #include "common/todo_error.hpp"
 
-OUTCOME_CPP_DEFINE_CATEGORY_3(fc, TodoError, e) {
+OUTCOME_CPP_DEFINE_CATEGORY_3(sgns, TodoError, e) {
   using E = sgns::TodoError;
   switch (e) {
     case E::ERROR:
