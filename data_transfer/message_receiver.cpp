78104a45190d1d6419a0860f103f41c95b4eebdb
kgcdream2019 <kgc.dream2019@gmail.com>
2020-06-24 09:17:25 +0800

    Added data_transfer/message_receiver.cpp

---
+++ data_transfer/message_receiver.cpp
@@ -0,0 +1,48 @@
+
+#include "data_transfer/message_receiver.hpp"
+
+namespace sgns::data_transfer {
+
+  outcome::result<void> MessageReceiver::registerVoucherType(
+      const std::string &type, std::shared_ptr<RequestValidator> validator) {
+    auto res = voucher_validators_.try_emplace(type, validator);
+    if (!res.second)
+      return MessageReceiverError::VOUCHER_VALIDATOR_ALREADY_REGISTERED;
+
+    return sgns::outcome::success();
+  }
+
+  outcome::result<void> MessageReceiver::validateVoucher(
+      const PeerId &sender, const DataTransferRequest &request) const {
+    auto validator = voucher_validators_.find(request.voucher_type);
+    if (validator == voucher_validators_.end()) {
+      return MessageReceiverError::VOUCHER_VALIDATOR_NOT_FOUND;
+    }
+    OUTCOME_TRY(base_cid, CID::fromString(request.base_cid));
+    OUTCOME_TRY(selector, IPLDNodeImpl::createFromRawBytes(request.selector));
+    if (request.is_pull) {
+      OUTCOME_TRY(validator->second->validatePull(
+          sender, request.voucher, base_cid, selector));
+    } else {
+      OUTCOME_TRY(validator->second->validatePush(
+          sender, request.voucher, base_cid, selector));
+    }
+
+    return outcome::success();
+  }
+
+}  // namespace sgns::data_transfer
+
+OUTCOME_CPP_DEFINE_CATEGORY_3(sgns::data_transfer, MessageReceiverError, e) {
+  using sgns::data_transfer::MessageReceiverError;
+
+  switch (e) {
+    case MessageReceiverError::VOUCHER_VALIDATOR_ALREADY_REGISTERED:
+      return "MessageReceiverError: voucher validator is already "
+             "registered";
+    case MessageReceiverError::VOUCHER_VALIDATOR_NOT_FOUND:
+      return "MessageReceiverError: voucher validator not found";
+  }
+
+  return "unknown error";
+}
