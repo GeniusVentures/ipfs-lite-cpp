05e7b6d21235ecb81cf8e69eb4a786637d1f118d
kgcdream2019 <kgc.dream2019@gmail.com>
2020-06-26 17:27:42 +0800

    Added data_transfer/impl

---
+++ data_transfer/impl/stream_message_sender.hpp
@@ -0,0 +1,29 @@
+
+#ifndef CPP_IPFS_LITE_DATA_TRANSFER_STREAM_MESSAGE_SENDER_HPP
+#define CPP_IPFS_LITE_DATA_TRANSFER_STREAM_MESSAGE_SENDER_HPP
+
+#include "data_transfer/message_sender.hpp"
+
+#include <libp2p/connection/stream.hpp>
+
+namespace sgns::data_transfer {
+
+  class StreamMessageSender : public MessageSender {
+   public:
+    explicit StreamMessageSender(
+        std::shared_ptr<libp2p::connection::Stream> stream);
+
+    outcome::result<void> sendMessage(
+        const DataTransferMessage &message) override;
+
+    outcome::result<void> close() override;
+
+    outcome::result<void> reset() override;
+
+   private:
+    std::shared_ptr<libp2p::connection::Stream> stream_;
+  };
+
+}  // namespace sgns::data_transfer
+
+#endif  // CPP_IPFS_LITE_DATA_TRANSFER_STREAM_MESSAGE_SENDER_HPP
