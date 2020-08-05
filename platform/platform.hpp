4b5643c031ef7c5528f4ac8bf8170d7774737c0f
ruymaster <mecury1984@outlook.com>
2020-08-05 16:53:19 +0800

    fix template error

--- platform/platform.hpp
+++ platform/platform.hpp
@@ -2,8 +2,11 @@
 #define PLATFORM_H
 
 #if defined(_MSC_VER)
-#include <BaseTsd.h>
-typedef SSIZE_T ssize_t;
+    #include <BaseTsd.h>
+    typedef SSIZE_T ssize_t;    
+    #define TEMPLATE_TO to
+#else
+    #define TEMPLATE_TO template to
 #endif
 
 #endif
